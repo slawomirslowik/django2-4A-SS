01324c294225d7c3aae7d9c30caa7a2cd6a26f23
Slawomir Slowik <SlawomirSlowik@yahoo.pl>
2024-10-17 09:01:56 +0200

    17.10.2024

---
+++ myapp/polls/kolekcja.py
@@ -0,0 +1,3 @@
+kol = ["mama", "tata", "dziadek", "babcia", "ciocia", "wujek", "bobas"]
+
+print([x for x in kol if x.startswith("b")])
--- myapp/polls/urls.py
+++ myapp/polls/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from mysite import views2
 
 urlpatterns = [
     path("", views.index, name="index"),
+        # ex: /polls/5/
+    path("<int:question_id>/", views2.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
